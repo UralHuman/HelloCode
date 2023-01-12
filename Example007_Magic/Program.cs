1af5ffa6b0c04fbfa19c5d725aa2c14215a5e263
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-12 22:37:47 +0500

    Починил треугольник Серпинского

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,12 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 40;
-int ya = 1;
-int xb = 1;
-int yb = 30;
-int xc = 80;
-int yc = 30;
+int xa = 30, ya = 1,
+    xb = 1,  yb = 30,
+    xc = 60, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -18,13 +15,14 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
+
 int x = xa, y = xb;
 
 int count = 0;
 
 while(count < 10000)
 {
-    int what = new Random().Next(0, 3); // [0;3) 0 1 2 
+    int what = new Random().Next(0, 3);
     if(what == 0)
     {
         x = (x + xa) / 2;
@@ -37,7 +35,7 @@ while(count < 10000)
         y = (y + yb) / 2;
     }
 
-    if(what == 2);
+    if(what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
@@ -45,5 +43,5 @@ while(count < 10000)
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
-    count = count + 1;
+    count++;
 }
