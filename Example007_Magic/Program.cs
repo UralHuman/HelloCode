5887b074bf8073f7caf11e0047e2ecccfcd1c128
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-12 22:41:20 +0500

    Починил треугольник Серпинского2

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 30, ya = 1,
+int xa = 40, ya = 1,
     xb = 1,  yb = 30,
-    xc = 60, yc = 30;
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
